11d8c15ee0cd2b4ca1e88612ac48d4a9a57f014f
Wanderson-Melo <75035633+Wanderson-Melo@users.noreply.github.com>
2020-12-28 14:42:14 -0300

    Adequação JavaScript

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,20 +1,41 @@
 var icone = document.getElementById('icone');
-
+//Válido para página Início
 function paraInic(){
-    icone.setAttribute('src', "/imagens/home.png");
+    icone.setAttribute('src', "imagens/home.png");
 }
 function paraEspec(){
-    icone.setAttribute('src', '/imagens/especificacoes.png');
+    icone.setAttribute('src', 'imagens/especificacoes.png');
 }
 function paraFotos(){
-    icone.setAttribute('src', '/imagens/fotos.png')
+    icone.setAttribute('src', 'imagens/fotos.png')
 }
 function paraMulti(){
-    icone.setAttribute('src', '/imagens/multimidia.png')
+    icone.setAttribute('src', 'imagens/multimidia.png')
 }
 function paraFale(){
-    icone.setAttribute('src', '/imagens/contato.png')
+    icone.setAttribute('src', 'imagens/contato.png')
 }
 function paraOculus(){
-    icone.setAttribute('src', '/imagens/glass-oculos-preto-peq.png')
+    icone.setAttribute('src', 'imagens/glass-oculos-preto-peq.png')
+}
+
+//Válido para demais páginas
+var icone0 = document.getElementById('icone0');
+function paraInic0(){
+    icone0.setAttribute('src', "../imagens/home.png");
+}
+function paraEspec0(){
+    icone0.setAttribute('src', '../imagens/especificacoes.png');
+}
+function paraFotos0(){
+    icone0.setAttribute('src', '../imagens/fotos.png')
+}
+function paraMulti0(){
+    icone0.setAttribute('src', '../imagens/multimidia.png')
+}
+function paraFale0(){
+    icone0.setAttribute('src', '../imagens/contato.png')
+}
+function paraOculus0(){
+    icone0.setAttribute('src', '../imagens/glass-oculos-preto-peq.png')
 }
