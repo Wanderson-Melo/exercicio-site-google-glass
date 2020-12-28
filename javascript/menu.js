c7cf062634d546160e9f92a8d40cd69e83de6dee
Wanderson-Melo <75035633+Wanderson-Melo@users.noreply.github.com>
2020-12-28 14:17:58 -0300

    Adicionado JavaScript

---
+++ javascript/menu.js
@@ -0,0 +1,20 @@
+var icone = document.getElementById('icone');
+
+function paraInic(){
+    icone.setAttribute('src', "/imagens/home.png");
+}
+function paraEspec(){
+    icone.setAttribute('src', '/imagens/especificacoes.png');
+}
+function paraFotos(){
+    icone.setAttribute('src', '/imagens/fotos.png')
+}
+function paraMulti(){
+    icone.setAttribute('src', '/imagens/multimidia.png')
+}
+function paraFale(){
+    icone.setAttribute('src', '/imagens/contato.png')
+}
+function paraOculus(){
+    icone.setAttribute('src', '/imagens/glass-oculos-preto-peq.png')
+}
